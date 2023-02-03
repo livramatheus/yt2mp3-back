0324b77b01c6def0339ee7b2131a17df646ede9b
livramateus <livra.matheus@gmail.com>
2023-02-02 22:39:52 -0300

    feat: develop API

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
+import * as dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import axios from "axios";
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
-app.get("/video", function(req: Request, res: Response) {
-    res.json({
-        "res": req.query.id
-    }).send();
+app.get("/video", async function(req: Request, res: Response) {
+    const requestOptions = {
+        method: 'GET',
+        url: 'https://youtube-mp36.p.rapidapi.com/dl',
+        params: { id: req.query.id },
+        headers: {
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Host': process.env.RAPID_API_HOST
+        }
+    }
+
+    const response = await axios.request(requestOptions);
+    res.json(response.data).send();
+
 });
 
 app.listen(process.env.PORT || 3333);
