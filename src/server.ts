abec06250c24bd233e00337680cf333078f3694f
livramateus <livra.matheus@gmail.com>
2023-02-02 21:40:15 -0300

    feat: develop app core

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Init1');
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/video", function(req: Request, res: Response) {
+    res.json({
+        "res": req.query.id
+    }).send();
+});
+
+app.listen(process.env.PORT || 3333);
