3e6845a3ddc0c17e16222962edc23111b4c7a772
livramateus <livra.matheus@gmail.com>
2023-02-07 20:46:54 -0300

    fix: fix status 429 crash

--- src/routes/video/VideoRoutes.ts
+++ src/routes/video/VideoRoutes.ts
@@ -9,7 +9,7 @@ const corsOptions = {
   optionsSuccessStatus: 200
 }
 
-VideoRouters.get("/video", cors(corsOptions),async function(req: Request, res: Response) {
+VideoRouters.get("/video", cors(corsOptions), async function(req: Request, res: Response): Promise<Response> {
     const requestOptions = {
         method: 'GET',
         url: 'https://youtube-mp36.p.rapidapi.com/dl',
@@ -19,10 +19,28 @@ VideoRouters.get("/video", cors(corsOptions),async function(req: Request, res: R
             'X-RapidAPI-Host': process.env.RAPID_API_HOST
         }
     }
+    if (requestOptions.params.id) {
+      try {
+        const response = await axios.request(requestOptions);
+        return res.json(response.data).status(response.status).send();
+      } catch (error: any) {
+        console.log(error.message);
 
-    const response = await axios.request(requestOptions);
-    res.json(response.data).send();
+        if (error.response.status === 429) {
+          return res.status(429).send({
+            message: "The daily download limit has been exceeded"
+          });
+        }
 
+        return res.status(500).send({
+          message: "Something went wrong with the server"
+        });
+      }
+    } else {
+      return res.status(400).send({
+        "message": "Video id missing"
+      });
+    }
 });
 
 export { VideoRouters };
